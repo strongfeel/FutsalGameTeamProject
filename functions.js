441ae0cae217f44244b57a5bf33afac94e51b978
Bzarre <32144764@dankook.ac.kr>
2024-09-19 20:32:48 +0900

    function update

--- functions.js
+++ functions.js
@@ -1,10 +1,86 @@
+import express from 'express';
+import { prisma } from '.../utils/prisma.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+function teampower(){
+    let team1 = 0
+    router.post('/playerteam', async (req, res, next) => {
+        try {
+           const { userId } = req.users;
+     
+           const roster = await prisma.rosters.findMany({
+              where: {
+                 select: +userId,
+              },
+     
+              select: {
+                 playerId: {
+                    select: {
+                       playerName: true,
+                       speed: true,
+                       goalDecision: true,
+                       goalPower: true,
+                       defence: true,
+                       stamina: true,
+                    },
+     
+                 },
+              },
+           });
+           return res.status(200), json({ data: roster });
+        } catch (err) {
+           next(err);
+        }
+     });
+     team1= speed * 0.1 + goalDecision * 0.2 + goalPower * 0.15 + defence * 0.3 + stamina * 0.25;
+
+     return team1;
+}
+
+function opponentpower() {
+    let team2 = 0;
+    router.post('/opponentteam', async (req, res, next) => {
+        try {
+           const { userId } = req.params;
+     
+           const roster = await prisma.rosters.findMany({
+              where: {
+                 select: +userId,
+              },
+     
+              select: {
+                 playerId: {
+                    select: {
+                       playerName: true,
+                       speed: true,
+                       goalDecision: true,
+                       goalPower: true,
+                       defence: true,
+                       stamina: true,
+                    },
+     
+                 },
+              },
+           });
+           return res.status(200), json({ data: roster });
+        } catch (err) {
+           next(err);
+        }
+     });
+     team2= speed * 0.1 + goalDecision * 0.2 + goalPower * 0.15 + defence * 0.3 + stamina * 0.25;
+
+     return team2;
+}
+
+
 function gameplay() {
     let round = 0;
     let score1, score2 = 0;
 
-    const diff = team1 - team2;
-    const chance1 = Math.round(Math.random() * (50 + diff));
-    const chance2 = Math.round(Math.random() * (50 - diff));
+    let diff = team1 - team2;
+    let chance1 = Math.round(50 + diff);
+    let chance2 = Math.round(50 - diff);
 
     while (round < 10){
         if(round % 2 ===0){
@@ -18,6 +94,7 @@ function gameplay() {
                 console.log('2Team이 득점했습니다!');
             };
         }
+        console.log(`${score1} : ${score2}`);
     };
 
     console.log(`경기 종료\n${score1} : ${score2}`);
@@ -26,5 +103,5 @@ function gameplay() {
     else if (score1 < score2)
         console.log('패배');
     else
-        console.log('무승부')
+        console.log('무승부');
 }
