725532d90d283e9853ff20e1b3e953a7c3fe396f
sdh02142 <sdh02142@naver.com>
2024-09-20 15:47:06 +0900

    {src/routes/cashshop.js, app.js} => 작성 완료, prisma/schema.prisma(가시성)

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express, { Router } from "express";
 import cookieParser from "cookie-parser";
 import LogMiddleware from "./src/middlewares/log.middleware.js";
 import errorHandlingMiddleware from "./src/middlewares/error-handling.middleware.js";
+
+import UsersRouter from "./src/routes/cashshop.js";
+
 import dotenv from 'dotenv';
+
 
 dotenv.config();
 
-// import UsersRouter from "./routes/users.routers.js";
 // import CharactersRouter from "./routes/characters.routers.js";
 // import ItemsRouter from "./routes/items.routers.js";
 const app = express();
@@ -14,8 +17,10 @@ const PORT = process.env.port;
 app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-// app.use("/api", [UsersRouter, CharactersRouter, ItemsRouter]);
 app.use(errorHandlingMiddleware);
+
+app.use("/api", [UsersRouter]);
+
 app.set("port", PORT);
 app.get("/", (req, res) => {
   res.send("Hello world!");
